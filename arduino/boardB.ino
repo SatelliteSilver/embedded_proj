f6af6eb1a332b276e02987fd6852982f4ee13899
wisang <42998574+SatelliteSilver@users.noreply.github.com>
2025-10-24 16:05:12 +0900

    Create boardB.ino
    default code

---
+++ arduino/boardB.ino
@@ -0,0 +1,31 @@
+// header
+#include <SoftwareSerial.h>
+
+// Serial Define
+SoftwareSerial BTSerial(2, 3);  //TX2/  RX3
+// "FOR DEBUGGING" 디버깅용: 6,7번을 시리얼 통신용으로 사용합니다.
+//SoftwareSerial mySerial(6, 7);
+
+void setup() {
+  // pinMode(8, OUTPUT);    // HC-05 핀 제어
+  // digitalWrite(8, HIGH); // AT 사용 용도
+
+  // for debugging
+  // mySerial.begin(9600);
+
+  Serial.begin(9600);   
+  BTSerial.begin(9600); //At+UART:변경속도,0,0 (115200으로 변경시 사용)
+  Serial.println("PC1 <-> PC2 bluetooth ready");  // ready
+}
+
+void loop() {
+  if (BTSerial.available()) {
+    int data = BTSerial.read();
+    Serial.write(data);
+  }
+
+  if (Serial.available()) {
+    int data = Serial.read();
+    BTSerial.write(data);
+  }
+}
